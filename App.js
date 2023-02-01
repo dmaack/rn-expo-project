33ca81ac258d1fc73ed57d29cefa5941a983108f
dmaack <dominiquemaack@gmail.com>
2023-01-31 21:11:03 -0700

    cleanup

--- App.js
+++ App.js
@@ -21,5 +21,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: { flex: 1, marginTop: StatusBar.currentHeight },
   search: { padding: 16, backgroundColor: "green" },
-  list: { flex: 1, padding: 16, backgroundColor: "blue" }
+  list: { flex: 1, padding: 16, backgroundColor: "blue" },
 });
